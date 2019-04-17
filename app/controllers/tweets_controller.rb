82ba231798661cfa4bb1fde8fd4971c8bdaf5a51
hiroaki122 <hiroaki@hiroaki.local>
2019-04-17 19:51:07 +0900

    ツイート詳細画面作成

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,5 @@
 class TweetsController < ApplicationController
-    before_action :move_to_index, except: :index
+    before_action :move_to_index, except: [:index, :show]
 
     def index
         @tweets = Tweet.includes(:user).page(params[:page]).per(5).order("created_at DESC")
@@ -28,6 +28,10 @@ class TweetsController < ApplicationController
         if tweet.user_id == current_user.id
           tweet.update(tweet_params)
         end
+    end
+
+    def show 
+        @tweet = Tweet.find(params[:id])
     end
 
     private
